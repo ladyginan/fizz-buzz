82b0f39d8cf0ccbc3e531dd408faeddedce3792c
Irina <irishkinson@list.ru>
2018-11-20 05:19:18 +0300

    change config add value from properties

--- back/src/main/java/app/conf/WebConfig.java
+++ back/src/main/java/app/conf/WebConfig.java
@@ -1,5 +1,6 @@
 package app.conf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${userClient.path}")
+    private String clientPath;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins(clientPath)
                 .allowedMethods("GET", "POST");
     }
 }
